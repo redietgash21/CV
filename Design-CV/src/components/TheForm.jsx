08798503c3bb09b51b0296f1fda0b9ef188a3249
redietgash21 <redietgash21@gmail.com>
2023-12-11 20:24:54 +0300

    cv

--- Design-CV/src/components/TheForm.jsx
+++ Design-CV/src/components/TheForm.jsx
@@ -6,7 +6,7 @@
 
 
 
-
+import "./TheForm.css"
 
 function TheForm () {
 
@@ -16,38 +16,43 @@ function TheForm () {
           <div className="formHeader">
             <button className="formBtn">Back to home</button>
           </div>
+          <hr />
           <div className="formMain">
-            <img src="" alt="" />
-          </div>
-          <div className="">
-            <p>First Name</p>
-            <input type="text" />
-            <p>Middle Name</p>
-            <input type="text" />
-            <p>Last Name</p>
-            <input type="text" />
-          </div>
-          <div className="">
-            <p>Age</p>
-            <input type="text" />
-            <p>Gmail</p>
-            <input type="text" />
-            <p>Address</p>
-            <input type="text" />
-            <p>Phone Number</p>
-            <input type="text" />
-          </div>
-          <div className="">
-            <p>About</p>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-            <p>Experiance</p>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-          </div>
-          <div className="">
-            <p>About</p>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-            <p>Experiance</p>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <div className="firstLine">
+              <div className="img">
+                <img src="" alt="" />
+              </div>
+              <div className="row">
+                <p>First Name</p>
+                <input type="text" />
+                <p>Middle Name</p>
+                <input type="text" />
+                <p>Last Name</p>
+                <input type="text" />
+              </div>
+            </div>
+            <div className="col">
+              <p>Age</p>
+              <input type="text" />
+              <p>Gmail</p>
+              <input type="text" />
+              <p>Address</p>
+              <input type="text" />
+              <p>Phone Number</p>
+              <input type="text" />
+            </div>
+            <div className="col">
+              <p>About</p>
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+              <p>Experiance</p>
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+            <div className="col">
+              <p>About</p>
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+              <p>Experiance</p>
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
           </div>
           <button>Download CV</button>
        </div>
