3a5374c6f11007f2fc3cd731c6835f6abd4eadce
redietgash21 <redietgash21@gmail.com>
2023-12-11 21:41:34 +0300

    cv

--- Design-CV/src/components/TheForm.jsx
+++ Design-CV/src/components/TheForm.jsx
@@ -20,41 +20,57 @@ function TheForm () {
           <div className="formMain">
             <div className="firstLine">
               <div className="img">
-                <img src="" alt="" />
+                <img src="" alt="upload photo" />
               </div>
               <div className="row">
-                <p>First Name</p>
-                <input type="text" />
-                <p>Middle Name</p>
-                <input type="text" />
-                <p>Last Name</p>
-                <input type="text" />
+                <div className="">
+                  <p>First Name</p>
+                  <input type="text" />
+                </div>
+                <div className="">
+                  <p>Middle Name</p>
+                  <input type="text" />
+                </div>
+                <div className="">
+                  <p>Last Name</p>
+                  <input type="text" />
+                </div>
               </div>
             </div>
-            <div className="col">
-              <p>Age</p>
-              <input type="text" />
-              <p>Gmail</p>
-              <input type="text" />
-              <p>Address</p>
-              <input type="text" />
-              <p>Phone Number</p>
-              <input type="text" />
-            </div>
-            <div className="col">
-              <p>About</p>
-              <textarea name="" id="" cols="30" rows="10"></textarea>
-              <p>Experiance</p>
-              <textarea name="" id="" cols="30" rows="10"></textarea>
-            </div>
-            <div className="col">
-              <p>About</p>
-              <textarea name="" id="" cols="30" rows="10"></textarea>
-              <p>Experiance</p>
-              <textarea name="" id="" cols="30" rows="10"></textarea>
+            <div className="secondLine">
+              <div className="colLeft">
+              <div className="">
+                  <p>Age</p>
+                  <input type="text" />
+                </div>
+                <div className="">
+                  <p>Gmail</p>
+                  <input type="text" />
+                </div>
+                <div className="">
+                  <p>Address</p>
+                  <input type="text" />
+                </div>
+                <div className="">
+                  <p>Phone Number</p>
+                  <input type="text" />
+                </div>
+              </div>
+              <div className="colCenter">
+                <p>About</p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <p>Experiance</p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+              </div>
+              <div className="colRight">
+                <p>About</p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <p>Experiance</p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+              </div>
             </div>
           </div>
-          <button>Download CV</button>
+          <button className="downloadBtn">Download CV</button>
        </div>
       </>
     )
