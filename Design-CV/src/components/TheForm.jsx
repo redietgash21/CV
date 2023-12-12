0265cd18725f640bc65b6af7850e64f486791bd2
redietgash21 <redietgash21@gmail.com>
2023-12-12 20:40:44 +0300

    cv

--- Design-CV/src/components/TheForm.jsx
+++ Design-CV/src/components/TheForm.jsx
@@ -7,8 +7,19 @@
 
 
 import "./TheForm.css"
+import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom"
 function TheForm () {
+   const [firstName, setFirstName]=useState("")
+   const [lastName, setLastName]=useState("")
+   const [middleName, setMiddleName]=useState("")
+   const [age, setAge]=useState(0)
+   const [gmail, setGmail]=useState("")
+   const [address, setAdress]=useState("")
+   const [phoneNumber, setPhoneNumber]=useState("")
+   const [about, setAbout]=useState("")
+   const [experiance, setExperiance]=useState("")
+   const [education, setEducation]=useState("")
 
     return (
       <>
@@ -27,15 +38,25 @@ function TheForm () {
               <div className="row">
                 <div className="">
                   <p>First Name</p>
-                  <input type="text" />
+                  <input type="text" 
+                          onChange={(event)=>{
+                          setFirstName(event.target.value)
+                         }}/>
                 </div>
                 <div className="">
                   <p>Middle Name</p>
-                  <input type="text" />
+                  <input type="text" 
+                          onChange={(event)=>{
+                            setMiddleName(event.target.value)
+                          }}/>
                 </div>
                 <div className="">
                   <p>Last Name</p>
-                  <input type="text" />
+                  <input type="text" 
+                         onChange={(event)=>{
+                          setLastName(event.target.value)
+                        }}
+                   />
                 </div>
               </div>
             </div>
@@ -43,30 +64,57 @@ function TheForm () {
               <div className="colLeft">
               <div className="">
                   <p>Age</p>
-                  <input type="text" />
+                  <input type="text" 
+                  onChange={(event)=>{
+                    setAge(event.target.value)
+                  }}/>
                 </div>
                 <div className="">
                   <p>Gmail</p>
-                  <input type="text" />
+                  <input type="text" 
+                         onChange={(event)=>{
+                          setGmail(event.target.value)
+                        }}
+                  />
                 </div>
                 <div className="">
                   <p>Address</p>
-                  <input type="text" />
+                  <input type="text" 
+                         onChange={(event)=>{
+                          setAdress(event.target.value)
+                        }}
+                  />
                 </div>
                 <div className="">
                   <p>Phone Number</p>
-                  <input type="text" />
+                  <input type="text" 
+                         onChange={(event)=>{
+                          setPhoneNumber(event.target.value)
+                        }}
+                  />
                 </div>
               </div>
               <div className="colCenter">
                 <p>About</p>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <textarea name="" id="" cols="30" rows="10"
+                          onChange={(event)=>{
+                            setAbout(event.target.value)
+                          }}
+                 ></textarea>
                 <p>Experiance</p>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <textarea name="" id="" cols="30" rows="10"
+                          onChange={(event)=>{
+                            setExperiance(event.target.value)
+                          }}
+                ></textarea>
               </div>
               <div className="colRight">
-                <p>About</p>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <p>Education</p>
+                <textarea name="" id="" cols="30" rows="10"
+                          onChange={(event)=>{
+                            setEducation(event.target.value)
+                          }}
+                ></textarea>
                 <p>Experiance</p>
                 <textarea name="" id="" cols="30" rows="10"></textarea>
               </div>
